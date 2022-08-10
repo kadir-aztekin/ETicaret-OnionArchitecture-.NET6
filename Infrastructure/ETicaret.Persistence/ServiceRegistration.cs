31402f5f450447bea049a519b17661a045a485ab
Kadir Aztekin <aztekadir@gmail.com>
2022-08-10 04:27:57 +0300

    ss

--- Infrastructure/ETicaret.Persistence/ServiceRegistration.cs
+++ Infrastructure/ETicaret.Persistence/ServiceRegistration.cs
@@ -1,11 +1,13 @@
 ﻿using ETicaret.Application.Abstactions;
 using ETicaret.Persistence.Concretes;
+using ETicaret.Persistence.Context;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace ETicaret.Persistence
 {
@@ -14,6 +16,8 @@ namespace ETicaret.Persistence
         public static void AddPersistenceServices(this IServiceCollection service)
         {
             service.AddSingleton<IProductService, ProductService>();
+            service.AddDbContext<ETicaretDbContext>(options => options.UseNpgsql(
+                "User ID=root;Password=1234;Host=localhost;Port=5432;Database=ETicaret;"));
         }
     }
 }
