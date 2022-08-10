11538556b63e97caec70335dcae3b263409e5528
Kadir Aztekin <aztekadir@gmail.com>
2022-08-10 04:19:50 +0300

    Db Updatae

---
+++ Core/ETicaret.Domain/Entites/Order.cs
@@ -0,0 +1,18 @@
+﻿using ETicaret.Domain.Entites.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaret.Domain.Entites
+{
+    public class Order : BaseEntity
+    {
+        public int CustomerId { get; set; }
+        public string Description { get; set; }
+        public string Address { get; set; }
+        public ICollection<Product> Products { get; set; }
+        public Customer Customer { get; set; }
+    }
+}
---
+++ Infrastructure/ETicaret.Persistence/Context/ETicaretDbContext.cs
@@ -0,0 +1,20 @@
+﻿using ETicaret.Domain.Entites;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaret.Persistence.Context
+{
+    public class ETicaretDbContext : DbContext
+    {
+        public ETicaretDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
